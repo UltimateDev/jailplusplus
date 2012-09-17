ded31325e3522079a5ef5c653de93e264dd68c29
Ariel Goldblatt <a.k.a.gldbltt@gmail.com>
2012-09-16 23:00:30 -0700

    fixed mkjail command

--- src/main/java/tk/ultimatedev/jailplusplus/commands/Test.java
+++ src/main/java/tk/ultimatedev/jailplusplus/commands/Test.java
@@ -9,7 +9,7 @@ import tk.ultimatedev.jailplusplus.util.Log;
 
 public class Test implements SubCommand {
     public boolean onCommand(CommandSender player, String[] args) {
-        if (args[1].equalsIgnoreCase("new")) {
+        if (args[0].equalsIgnoreCase("mkjail")) {
             Log.info("debug");
             Jail newJail = new Jail("Test jail", new Cuboid(new Location(Bukkit.getWorld("world"), 1, 64, 3), new Location(Bukkit.getWorld("world"), 3, 64, 5)));
 
