a4c0f080a8cbf0853f04ea5e0f5a9f25bc13dd83
Ariel Goldblatt <a.k.a.gldbltt@gmail.com>
2012-09-16 17:52:11 -0700

    fixed NPE

--- src/main/java/tk/ultimatedev/jailplusplus/ExceptionHandler.java
+++ src/main/java/tk/ultimatedev/jailplusplus/ExceptionHandler.java
@@ -1,28 +1,26 @@
 package tk.ultimatedev.jailplusplus;
 
-import java.util.logging.Logger;
 import org.bukkit.plugin.Plugin;
+import tk.ultimatedev.jailplusplus.util.Log;
 
 /**
  * @author YoshiGenius
  */
 public class ExceptionHandler {
-    
     Plugin plugin;
-    final Logger log = plugin.getLogger();
-    
+
     public ExceptionHandler(Plugin plugin) {
         this.plugin = plugin;
     }
-    
+
     public void logException(Exception exception) {
-        log.info("ERROR: Post all of this on the " + plugin.getName() + " BukkitDev page!");
-        log.info("ERROR TYPE: " + exception.getCause());
-        log.info("ERROR MSG: " + exception.getMessage());
-        log.info("---STACK TRACE START---"); 
+        Log.info("ERROR: Post all of this on the " + plugin.getName() + " BukkitDev page!");
+        Log.info("ERROR TYPE: " + exception.getCause());
+        Log.info("ERROR MSG: " + exception.getMessage());
+        Log.info("---STACK TRACE START---");
         exception.printStackTrace();
-        log.info("---STACK TRACE FINISH---");
-        log.info("ERROR: Post all of this on the " + plugin.getName() + " Bukkit Dev page in a ticket or a comment.");
+        Log.info("---STACK TRACE FINISH---");
+        Log.info("ERROR: Post all of this on the " + plugin.getName() + " Bukkit Dev page in a ticket or a comment.");
     }
 
 }
