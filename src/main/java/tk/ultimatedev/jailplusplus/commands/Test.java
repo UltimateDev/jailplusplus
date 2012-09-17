400e3e822bfca396aee20de4c1f93796e1f605f4
Ariel Goldblatt <a.k.a.gldbltt@gmail.com>
2012-09-16 22:42:37 -0700

    fuq da polic3

--- src/main/java/tk/ultimatedev/jailplusplus/commands/Test.java
+++ src/main/java/tk/ultimatedev/jailplusplus/commands/Test.java
@@ -10,29 +10,25 @@ import tk.ultimatedev.jailplusplus.util.Log;
 public class Test implements SubCommand {
     public boolean onCommand(CommandSender player, String[] args) {
         if (args[1].equalsIgnoreCase("new")) {
-            if (args[2].equalsIgnoreCase("jail")) {
-                Log.info("debug");
-                Jail newJail = new Jail("Test jail", new Cuboid(new Location(Bukkit.getWorld("world"), 1, 64, 3), new Location(Bukkit.getWorld("world"), 3, 64, 5)));
+            Log.info("debug");
+            Jail newJail = new Jail("Test jail", new Cuboid(new Location(Bukkit.getWorld("world"), 1, 64, 3), new Location(Bukkit.getWorld("world"), 3, 64, 5)));
 
-                switch (newJail.save()) {
-                    case ALREADY_EXISTS:
-                        Log.info("Already exists");
-                        break;
-                    case ALREADY_SAVED:
-                        Log.info("Already saved");
-                        break;
-                    case SUCCESS:
-                        Log.info("Success");
-                        break;
-                    case FAILURE:
-                        Log.info("Failure");
-                        break;
-                    default:
-                        Log.info("Default");
-                        break;
-                }
-            } else {
-                Log.info(args[2]);
+            switch (newJail.save()) {
+                case ALREADY_EXISTS:
+                    Log.info("Already exists");
+                    break;
+                case ALREADY_SAVED:
+                    Log.info("Already saved");
+                    break;
+                case SUCCESS:
+                    Log.info("Success");
+                    break;
+                case FAILURE:
+                    Log.info("Failure");
+                    break;
+                default:
+                    Log.info("Default");
+                    break;
             }
         } else {
             Log.info(args[1]);
