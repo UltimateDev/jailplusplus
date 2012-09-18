99fb99091888eff931402740ccaf364a72aa7f8b
Ariel Goldblatt <a.k.a.gldbltt@gmail.com>
2012-09-17 18:35:05 -0700

    fixed compile error

--- src/main/java/tk/ultimatedev/jailplusplus/task/UnjailTask.java
+++ src/main/java/tk/ultimatedev/jailplusplus/task/UnjailTask.java
@@ -21,7 +21,7 @@ public class UnjailTask implements Runnable {
                     if (prisoner.getX() == -1 && prisoner.getY() == -1 && prisoner.getZ() == -1) {
                         Bukkit.getPlayer(prisoner.getPlayer()).teleport(Cell.getCell(prisoner.getCell()).getJail().getCuboid().getWorld().getSpawnLocation());
                     } else {
-                        Bukkit.getPlayer(prisoner.getPlayer()).teleport(new Location(Bukkit.getWorld(prisoner.getWorld()), (double) prisoner.getX(), (double) prisoner.getY(), (double) prisoner.getZ())))
+                        Bukkit.getPlayer(prisoner.getPlayer()).teleport(new Location(Bukkit.getWorld(prisoner.getWorld()), (double) prisoner.getX(), (double) prisoner.getY(), (double) prisoner.getZ()));
                     }
 
                     Messenger.sendMessage(Bukkit.getPlayer(prisoner.getPlayer()), "You have served your sentence, and you are now unjailed!");
