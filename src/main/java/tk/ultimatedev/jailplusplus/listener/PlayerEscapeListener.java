c156559ddd4dd8ab71b3a79c045ca202f7dea9e3
Ariel Goldblatt <a.k.a.gldbltt@gmail.com>
2012-09-17 18:37:09 -0700

    skleleton player escape listener

---
+++ src/main/java/tk/ultimatedev/jailplusplus/listener/PlayerEscapeListener.java
@@ -0,0 +1,7 @@
+package tk.ultimatedev.jailplusplus.listener;
+
+import org.bukkit.event.Listener;
+
+public class PlayerEscapeListener implements Listener {
+    // Put listeners here that prevent players from escaping from a jail
+}
