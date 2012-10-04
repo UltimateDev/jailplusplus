94a73f2fc9a46f392299a14f5d19782cbd3dda90
YoshiGenius <yoshigenius@gmail.com>
2012-10-04 11:15:34 +1000

    JailAPI

---
+++ src/main/java/tk/ultimatedev/jailplusplus/api/JailAPI.java
@@ -0,0 +1,30 @@
+package tk.ultimatedev.jailplusplus.api;
+
+import org.bukkit.entity.Player;
+import tk.ultimatedev.jailplusplus.models.Prisoner;
+
+/**
+ * @author YoshiGenius
+ */
+public class JailAPI {
+    
+    public boolean isJailed(Player player) {
+        Prisoner prisoner = Prisoner.getPrisoner(player.getName());
+        if (prisoner == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public void setJailed(Player player, boolean jailed) {
+        Prisoner prisoner = Prisoner.getPrisoner(player.getName());
+        if (isJailed(player) && jailed) {
+            return;
+        }
+        if (!(isJailed(player)) && !(jailed)) {
+            return;
+        }
+    }
+
+}
